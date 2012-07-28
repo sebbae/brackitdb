b2fef042d16aa135d4dd76f5d7632944b09caead
Sebastian Baechle <devnull@localhost>
2012-07-28 13:12:50 +0200

    simplify attribute access

--- brackitdb-server/src/main/java/org/brackit/server/node/bracket/BracketNode.java
+++ brackitdb-server/src/main/java/org/brackit/server/node/bracket/BracketNode.java
@@ -48,11 +48,9 @@ import org.brackit.server.store.index.bracket.HintPageInformation;
 import org.brackit.server.store.index.bracket.InsertController;
 import org.brackit.server.store.index.bracket.NavigationMode;
 import org.brackit.server.store.index.bracket.StreamIterator;
+import org.brackit.server.store.index.bracket.filter.AttrFilter;
 import org.brackit.server.store.index.bracket.filter.BracketFilter;
-import org.brackit.server.store.index.bracket.filter.ChildPathNodeTypeFilter;
 import org.brackit.server.store.index.bracket.filter.ElementFilter;
-import org.brackit.server.store.index.bracket.filter.NodeTypeFilter;
-import org.brackit.server.store.index.bracket.filter.PSNodeFilter;
 import org.brackit.server.store.index.bracket.filter.TextFilter;
 import org.brackit.server.store.page.bracket.RecordInterpreter;
 import org.brackit.server.tx.Tx;
@@ -396,19 +394,21 @@ public class BracketNode extends TXNode<BracketNode> {
 
 	@Override
 	public BracketNode getAttributeInternal(QNm name) throws DocumentException {
-
-		// look for the requested attribute PSNode
-		PSNode attributePSNode = locator.pathSynopsis.getChildIfExists(
-				psNode.getPCR(), name, Kind.ATTRIBUTE.ID, null);
-
-		if (attributePSNode == null) {
-			return null;
-		}
-
+//		// look for the requested attribute PSNode
+//		PSNode attributePSNode = locator.pathSynopsis.getChildIfExists(
+//				psNode.getPCR(), name, Kind.ATTRIBUTE.ID, null);
+//
+//		if (attributePSNode == null) {
+//			return null;
+//		}
+//
+//		PSNodeFilter filter = new PSNodeFilter(locator.pathSynopsis, attributePSNode,
+//				true);
+		BracketFilter filter = new AttrFilter(null, name, null);		
+		
 		Stream<BracketNode> aStream = locator.collection.store.index
 				.openAttributeStream(locator, deweyID, hintPageInfo,
-						new PSNodeFilter(locator.pathSynopsis, attributePSNode,
-								true));
+						filter);
 
 		BracketNode attribute = aStream.next();
 		aStream.close();
